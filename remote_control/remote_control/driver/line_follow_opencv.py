60193f291858dafa4ab92afb6a16b1ae0a243ff9
wangyuyyt <yuhomephone87@gmail.com>
2021-12-11 19:28:58 -0800

    minor fix and add debugging

--- remote_control/remote_control/driver/line_follow_opencv.py
+++ remote_control/remote_control/driver/line_follow_opencv.py
@@ -19,18 +19,21 @@ def detect_lane(image):
 
 def detect_edges(image):
     # Convert the image color to HSV
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) 
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    #cv2.imwrite('/home/pi/Pictures/hsv.jpg', hsv)
     
     # Lift blue
-    blue_lower = numpy.array([60, 40, 40])
-    blue_upper = numpy.array([150, 255, 255])
+    blue_lower = numpy.array([80, 40, 40])
+    blue_upper = numpy.array([170, 255, 255])
     blue = cv2.inRange(hsv, blue_lower, blue_upper)
+    #cv2.imwrite('/home/pi/Pictures/blue.jpg', blue)
 
     # Detect edges
     edges = cv2.Canny(blue, 100, 200)
+    #cv2.imwrite('/home/pi/Pictures/edges.jpg', edges)
 
     # Only focus bottom half of the screen
-    # Create mask for bottom half
+    # Create mask for upper half
     height, width = edges.shape
     mask = numpy.zeros_like(edges)
 
@@ -41,6 +44,7 @@ def detect_edges(image):
 
     cv2.fillPoly(mask, polygon, 255)
     filtered_edges = cv2.bitwise_and(edges, mask)
+    #cv2.imwrite('/home/pi/Pictures/filtered_edges.jpg', filtered_edges)
     return filtered_edges
 
 def detect_line_segments(edges):
@@ -82,6 +86,7 @@ def merge_to_lanes(image, lines):
             if x1 > right_region_boundary and x2 > right_region_boundary:
                 right.append((slope, y_int))
                 cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 5)
+    #cv2.imwrite('/home/pi/Pictures/line_segments.jpg', image)
 
     lanes = []
     if len(left) > 0:
@@ -96,6 +101,7 @@ def merge_to_lanes(image, lines):
     for lane in lanes:
         x1, y1, x2, y2 = lane
         cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 5)
+    #cv2.imwrite('/home/pi/Pictures/lane.jpg', image)
     return lanes
 
 def make_points(image, average): 
@@ -105,8 +111,8 @@ def make_points(image, average):
     y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
 
     # bound the coordinates within the frame
-    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
-    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+    x1 = int((y1 - intercept) / slope))
+    x2 = int((y2 - intercept) / slope))
     return [x1, y1, x2, y2]
 
 def lanes_to_angle(image, lanes):
@@ -138,11 +144,12 @@ def lanes_to_angle(image, lanes):
 
     # Display direction line
     cv2.line(image, (int(width/2), height), (int(x_offset + mid), int(height/2)), (255, 255, 0), 5)
+    #cv2.imwrite('/home/pi/Pictures/direction.jpg', image)
     return steering_angle
 
 
 def main():
-    image = cv2.imread('/home/pi/Pictures/Webcam/1.jpg')
+    image = cv2.imread('/home/pi/Pictures/Webcam/13.jpg')
     follow_line(image, None, None, None)
     cv2.imshow('image',image)
     cv2.waitKey(0)
